487408c04de89bfb887a0eb77bb91cc6ac917fa9
Devansh Rajput <rajputdevansh012@gmail.com>
2024-06-05 23:25:03 +0530

    Create Model Schema

---
+++ server/models/UserModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide your email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide your password"],
+    },
+    profile_pic: {
+      type: String,
+      default: "default.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
